257d0198275cbdc97313d96a87c443805280f5ec
monica <monica.parrillo@gmail.com>
2021-02-19 18:02:46 -0600

    styling on cards/materialize

--- src/components/BreadBox.js
+++ src/components/BreadBox.js
@@ -1,8 +1,40 @@
 import React from 'react';
-
-const BreadBox = ({}) => {
+import {Card, CardActionArea, CardMedia, Typography, CardContent, CardActions, Button} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+const BreadBox = ({imageurl, name, commentCount, description}) => {
+  const classes = useStyles()
   return (
-    <div></div>
+    <Card className={classes.root} variant="outlined">
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="imageurl"
+          title={name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Comment
+        </Button>
+
+      </CardActions>
+    </Card>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,31 @@
-import {Link} from 'react-router-dom'
 import {useEffect, useState} from 'react'
 import Header from './Header'
+import BreadBox from './BreadBox'
 import {getAllBread} from '../services/bread.service'
 
 const Home = () => {
   const [breadData, setBreadData] = useState()
   useEffect(()=>{
     getAllBread().then(res=>{
-      setBreadData(res.data)
+      setBreadData(res.data.breads)
     })
   },[])
-  const display = () => {
 
+  const display = () => {
+    return (breadData && breadData.map(bread => {
+      return <BreadBox
+      name={bread.name}
+      imageurl={bread.imageurl}
+      key={bread.id}
+      commentCount={bread.commentCount}/>
+    }))
 }
 
 return (
+  <>
   <Header/>
+   {display()}
+  </>
 )
 }
 
